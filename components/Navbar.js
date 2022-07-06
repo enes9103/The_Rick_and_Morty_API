2a2dac85f7d24ea61be21f1786f19ee362f98551
EnesUnlu <enes9103@gmail.com>
2022-07-07 01:30:02 +0300

    home page changed

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,36 +1,31 @@
 import styles from "../styles/Navbar.module.css"
 import Image from "next/image"
 import logo from "../public/logo-2.png"
+import Link from "next/link"
 
 
 const Navbar = () => {
   return (
     <div className={styles.navContainer}>
-    <div className={styles.leftNav}>
-      <a href="/">
-        <Image src={logo} alt="logo" height={80} width={220} />
-      </a>
+      <div className={styles.leftNav}>
+        <a href="/">
+          <Image src={logo} alt="logo" height={80} width={220} />
+        </a>
+      </div>
+      <div className={styles.rightNav}>
+        <Link href="/" >
+          <a className={styles.link}>HOME</a>
+        </Link>
+
+        <Link href="/episodes">
+          <a className={styles.link}>EPISODES</a>
+        </Link>
+
+        <Link href="/about">
+          <a className={styles.link}>ABOUT</a>
+        </Link>
+      </div>
     </div>
-    <div className={styles.rightNav}>
-      <nav>
-        <span>
-          <a href="/" className={styles.link}>
-            HOME
-          </a>
-        </span>
-        <span>
-          <a href="/episodes" className={styles.link}>
-            EPISODES
-          </a>
-        </span>
-        <span>
-          <a href="/about" className={styles.link}>
-            ABOUT
-          </a>
-        </span>
-      </nav>
-    </div>
-  </div>
   )
 }
 
