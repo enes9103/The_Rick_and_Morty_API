dc59736a66d0cd65cf1e3e6a56bb453dfa31cde6
EnesUnlu <enes9103@gmail.com>
2022-07-06 12:17:56 +0300

    Navbar created

---
+++ the_rich_and_morty_api/components/Navbar.js
@@ -0,0 +1,37 @@
+import styles from "../styles/Navbar.module.css"
+
+const Navbar = () => {
+  return (
+    <div className={styles.navContainer}>
+    <div className={styles.leftNav}>
+      <a href="/">
+        {/* <Image src={logo} alt="logo" width={100} height={100} /> */}
+      </a>
+      <a className={styles.title} href="/">
+        <p>Rick and Morty</p>
+      </a>
+    </div>
+    <div className={styles.rightNav}>
+      <nav>
+        <span>
+          <a href="/" className={styles.link}>
+            HOME
+          </a>
+        </span>
+        <span>
+          <a href="/" className={styles.link}>
+            CHARACTERS
+          </a>
+        </span>
+        <span>
+          <a href="/blog" className={styles.link}>
+            BLOG
+          </a>
+        </span>
+      </nav>
+    </div>
+  </div>
+  )
+}
+
+export default Navbar
